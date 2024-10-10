23741c15dda363664868dc21cb6d1595293a7a94
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2024-10-10 20:19:19 -0300

    Creation of PickableObject (empty MonoBehaviour) and added ability to pick an PickableObject to move it around the scene (SpawnOnClick will probably need a rename).

--- Assets/SharedAssets/Scripts/Input/SpawnOnClick.cs
+++ Assets/SharedAssets/Scripts/Input/SpawnOnClick.cs
@@ -23,7 +23,8 @@ namespace Project
 		[Space, SerializeField] private UnityEvent<GameObject> _onSpawned;
 		[Space, SerializeField] private UnityEvent<GameObject> _onEndPositioning;
 
-		private GameObject _instantiatedObject;
+		private GameObject _currentObject;
+		private Vector3 _pointerOffset;
 		private Dictionary<GameObject, LayerMask> _cachedLayerMasks;
 
 		#region Input
@@ -62,7 +63,12 @@ namespace Project
 		{
 			if (context.action.WasPressedThisFrame())
 			{
-				InstantiateObject(GetPointerRay());
+				Ray ray = GetPointerRay();
+				
+				if (TryPickObject(ray))
+					return;
+
+				InstantiateObject(ray);
 				return;
 			}
 
@@ -70,7 +76,7 @@ namespace Project
 			{
 				MoveInstantiatedObject(GetPointerRay());
 				ApplyCachedLayerMasks();
-				Debug.DrawLine(Camera.main.transform.position, _instantiatedObject.transform.position, Color.green, 10);
+				Debug.DrawLine(Camera.main.transform.position, _currentObject.transform.position, Color.green, 10);
 				Notify_OnEndPositiong();
 			}
 		}
@@ -165,6 +171,28 @@ namespace Project
 		#endregion
 
 		#region Instantiated Object
+		private bool TryPickObject(Ray ray)
+		{
+			if (Physics.Raycast(ray, out RaycastHit hit))
+			{
+				PickableObject pickedObject = hit.transform.GetComponentInChildren<PickableObject>();
+				if (!pickedObject) pickedObject = hit.transform.GetComponentInParent<PickableObject>();
+				if (!pickedObject) return false;
+
+				_currentObject = pickedObject.gameObject;
+				Collider[] colliders = _currentObject.GetComponentsInChildren<Collider>();
+				CacheLayerMasks(colliders);
+				ApplyIgnoreLayerMask(colliders);
+
+				if (Physics.Raycast(ray, out hit))
+					_pointerOffset = _currentObject.transform.position - hit.point;
+
+				return true;
+			}
+
+			return false;
+		}
+
 		private void InstantiateObject(Ray ray)
 		{
 			if (Physics.Raycast(ray, out RaycastHit hit))
@@ -172,8 +200,9 @@ namespace Project
 				Vector3 spawnPosition = hit.point;
 				Debug.Log($"spawn at position {spawnPosition} (hit object {hit.transform.name})");
 				Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red, 10);
-				_instantiatedObject = Instantiate(_prefabToSpawn, spawnPosition, _prefabToSpawn.transform.rotation, _parent);
-				Collider[] colliders = _instantiatedObject.GetComponentsInChildren<Collider>();
+				_currentObject = Instantiate(_prefabToSpawn, spawnPosition, _prefabToSpawn.transform.rotation, _parent);
+				_pointerOffset = Vector3.zero;
+				Collider[] colliders = _currentObject.GetComponentsInChildren<Collider>();
 				CacheLayerMasks(colliders);
 				ApplyIgnoreLayerMask(colliders);
 				Notify_OnSpawned();
@@ -182,33 +211,33 @@ namespace Project
 
 		private void MoveInstantiatedObject(Ray ray)
 		{
-			if (!_instantiatedObject) return;
+			if (!_currentObject) return;
 			if (Physics.Raycast(ray, out RaycastHit hit))
 			{
-				Vector3 newPosition = hit.point;
+				Vector3 newPosition = hit.point + _pointerOffset;
 				Debug.DrawLine(Camera.main.transform.position, hit.point, Color.blue, 1);
-				_instantiatedObject.transform.position = newPosition;
+				_currentObject.transform.position = newPosition;
 			}
 		}
 
 		private void RotateObject(bool clockwise)
 		{
-			if (!_instantiatedObject) return;
-			_instantiatedObject.transform.Rotate(new Vector3(0, clockwise ? 90 : -90, 0));
+			if (!_currentObject) return;
+			_currentObject.transform.Rotate(new Vector3(0, clockwise ? 90 : -90, 0));
 		}
 		#endregion
 
 		#region Notifiers
 		private void Notify_OnSpawned()
 		{
-			_onSpawned?.Invoke(_instantiatedObject);
-			OnSpawned?.Invoke(_instantiatedObject);
+			_onSpawned?.Invoke(_currentObject);
+			OnSpawned?.Invoke(_currentObject);
 		}
 
 		private void Notify_OnEndPositiong()
 		{
-			_onEndPositioning?.Invoke(_instantiatedObject);
-			OnEndPositioning?.Invoke(_instantiatedObject);
+			_onEndPositioning?.Invoke(_currentObject);
+			OnEndPositioning?.Invoke(_currentObject);
 		}
 		#endregion
 	}
---
+++ Assets/SharedAssets/Scripts/_Core/Runtime/PickableObject.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+namespace Project
+{
+	public class PickableObject : MonoBehaviour { }
+}
