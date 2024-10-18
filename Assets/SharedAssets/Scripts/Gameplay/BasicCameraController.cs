788978d874e9c5a03b4ab58af32a8c59c03aa80e
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2024-10-18 19:06:51 -0300

    Implemented basic camera rotation. Renamed BasicCameraMovementController to BasicCameraController.

--- Assets/SharedAssets/Scripts/Gameplay/BasicCameraController.cs
+++ Assets/SharedAssets/Scripts/Gameplay/BasicCameraController.cs
@@ -6,23 +6,35 @@ using UnityEngine.InputSystem;
 
 namespace Project
 {
-	public class BasicCameraMovementController : MonoBehaviour
+	public class BasicCameraController : MonoBehaviour
 	{
 		[SerializeField] private string _moveActionName = "Player/Move";
 		[SerializeField] private string _lookActionName = "Player/Look";
 		[Space]
 		[SerializeField] private Camera _camera;
 		[SerializeField] private GameObject _focusObject;
-		[SerializeField] private float _maxSpeed = 5;
+		[Header("Move Settings")]
+		[SerializeField] private float _maxMoveSpeed = 5;
 		[Min(0)]
-		[SerializeField] private float _smoothTime = .5f;
+		[SerializeField] private float _moveSmoothTime = .5f;
+		[Header("Look Settings")]
+		[Range(0, 3)]
+		[SerializeField] private float _rotationSensitivity = .5f;
+		[Range(0, 10)]
+		[SerializeField] private float _rotationSmoothTime = 5f;
+		[Range(-180, 180)]
+		[SerializeField] private float _minPitchAngle = 10;
+		[Range(-180, 180)]
+		[SerializeField] private float _maxPitchAngle = 90;
 
 		private InputAction _moveAction;
 		private InputAction _lookAction;
 
-		private Vector3 _currentVelocity;
 		private Vector2 _moveInput;
 		private Vector2 _lookInput;
+
+		private Vector3 _currentVelocity;
+		private Vector3 _targetRotation;
 
 		private void Reset()
 		{
@@ -35,11 +47,46 @@ namespace Project
 			_moveAction = InputSystem.actions.FindAction(_moveActionName);
 			_lookAction = InputSystem.actions.FindAction(_lookActionName);
 
+			_targetRotation = _focusObject.transform.rotation.eulerAngles;
 		}
 
 		private void Start()
 		{
 			ValidateParentConstraint();
+		}
+
+		private void OnEnable()
+		{
+			if (_moveAction != null)
+			{
+				_moveAction.performed += MoveAction_performed;
+				_moveAction.canceled += MoveAction_canceled;
+			}
+			if (_lookAction != null)
+			{
+				_lookAction.performed += LookAction_performed;
+				_lookAction.canceled += LookAction_canceled;
+			}
+		}
+
+		private void OnDisable()
+		{
+			if (_moveAction != null)
+			{
+				_moveAction.performed -= MoveAction_performed;
+				_moveAction.canceled -= MoveAction_canceled;
+			}
+			if (_lookAction != null)
+			{
+				_lookAction.performed -= LookAction_performed;
+				_lookAction.canceled -= LookAction_canceled;
+			}
+		}
+
+		private void Update()
+		{
+			Move();
+			Look();
 		}
 
 		private void ValidateParentConstraint()
@@ -80,35 +127,11 @@ namespace Project
 			}
 		}
 
-		private void OnEnable()
-		{
-			if (_moveAction != null)
-			{
-				_moveAction.performed += MoveAction_performed;
-				_moveAction.canceled += MoveAction_canceled;
-			}
-			if (_lookAction != null) _lookAction.performed += LookAction_performed;
-		}
-
-		private void OnDisable()
-		{
-			if (_moveAction != null)
-			{
-				_moveAction.performed -= MoveAction_performed;
-				_moveAction.canceled -= MoveAction_canceled;
-			}
-			if (_lookAction != null) _lookAction.performed -= LookAction_performed;
-		}
-
-		private void Update()
-		{
-			Move();
-			Look();
-		}
-
 		private void MoveAction_performed(InputAction.CallbackContext context) => CacheInput(context, ref _moveInput);
 		private void MoveAction_canceled(InputAction.CallbackContext context) => CacheInput(context, ref _moveInput);
+
 		private void LookAction_performed(InputAction.CallbackContext context) => CacheInput(context, ref _lookInput);
+		private void LookAction_canceled(InputAction.CallbackContext context) => CacheInput(context, ref _lookInput);
 
 		private void CacheInput<T>(InputAction.CallbackContext context, ref T cache) where T : struct
 		{
@@ -123,14 +146,16 @@ namespace Project
 			Debug.DrawLine(_focusObject.transform.position, _focusObject.transform.position + right, Color.red, .1f);
 
 			Vector3 current = _focusObject.transform.position;
-			Vector3 delta = (forward * _moveInput.y + right * _moveInput.x) * _maxSpeed;
+			Vector3 delta = (forward * _moveInput.y + right * _moveInput.x) * _maxMoveSpeed;
 			Vector3 target = current + delta;
-			_focusObject.transform.position = Vector3.SmoothDamp(current, target, ref _currentVelocity, _smoothTime, _maxSpeed, Time.deltaTime);
+			_focusObject.transform.position = Vector3.SmoothDamp(current, target, ref _currentVelocity, _moveSmoothTime, _maxMoveSpeed, Time.deltaTime);
 		}
 
 		private void Look()
 		{
-
+			_targetRotation += new Vector3(_lookInput.y, _lookInput.x,0) * _rotationSensitivity;
+			_targetRotation.x = Mathf.Clamp(_targetRotation.x, _minPitchAngle, _maxPitchAngle);
+			_focusObject.transform.rotation = Quaternion.Slerp(_focusObject.transform.rotation, Quaternion.Euler(_targetRotation), Time.deltaTime * _rotationSmoothTime);
 		}
 
 	}
